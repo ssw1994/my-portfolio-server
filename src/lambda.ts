4faac8851dc784c4ea3ef1db5d289390593e4962
unknown <waghmare.sachin1994@gmail.com>
2024-11-07 23:42:36 +0530

    setup netlify functions

---
+++ src/lambda.ts
@@ -0,0 +1,22 @@
+import { Handler, Context, Callback } from 'aws-lambda';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import serverlessExpress from 'serverless-http';
+
+let server: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress(expressApp);
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
